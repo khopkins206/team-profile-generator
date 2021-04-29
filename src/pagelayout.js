cb6886e53a845b8bc91d285eddade3b4d015fcdc
Katherine Jackson <katherine.jackson206@gmail.com>
2021-04-28 23:21:15 -0500

    added html for card creation

---
+++ src/pagelayout.js
@@ -0,0 +1,89 @@
+const createManager = (teamMember) => {
+  return `<div class="card text-center">
+      <div class="card-header">
+       Manager: ${teamMember.grabName()}
+     </div>
+      <div class="card-body">
+       <ul>
+        <li>ID: ${teamMember.grabID()} </li>
+        <li>Email: ${teamMember.grabEmail()} </li>
+       </ul>
+     </div>
+     <div class="card-footer text-muted">
+     </div>
+    </div>`
+}
+
+const createEngineer = (teamMember) => {
+  return `<div class="card text-center">
+      <div class="card-header">
+       Engineer: ${teamMember.grabName()}
+     </div>
+      <div class="card-body">
+       <ul>
+        <li>ID: ${teamMember.grabID()} </li>
+        <li>Email: ${teamMember.grabEmail()} </li>
+       </ul>
+     </div>
+     <div class="card-footer text-muted">
+     </div>
+    </div>`
+}
+
+const createIntern = (teamMember) => {
+  return `<div class="card text-center">
+      <div class="card-header">
+       Intern: ${teamMember.grabName()}
+     </div>
+      <div class="card-body">
+       <ul>
+        <li>ID: ${teamMember.grabID()} </li>
+        <li>Email: ${teamMember.grabEmail()} </li>
+       </ul>
+     </div>
+     <div class="card-footer text-muted">
+     </div>
+    </div>`
+}
+
+const generateHTML = (teamMembers) => {
+  console.log(teamMembers)
+
+  let cardContainer = ""
+
+  for (let index = 0; index < teamMembers.length; index++) {
+    const teamMember = teamMembers[index];
+    if (teamMember.grabPosition() === `Manager`) {
+      cardContainer = cardContainer + createManager(teamMember)
+    } else if (teamMember.grabPosition() === `Engineer`) {
+      cardContainer = cardContainer + createEngineer(teamMember)
+    } else if (teamMember.grabPosition() === `Intern`) {
+      cardContainer = cardContainer + createIntern(teamMember)
+    }
+  }
+
+  return `
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>My Team</title>
+    </head>
+    <body>
+        <nav>
+            <div class="nav-wrapper">
+            <a href="#" class="brand-logo center">My Team</a>
+            </div>
+        </nav>
+        <div class="row">
+        ${cardContainer}
+        </div>
+        </div>
+    </body>
+      <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js" integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf" crossorigin="anonymous"></script>
+    </html>`;
+};
+
+module.exports = generateHTML;
